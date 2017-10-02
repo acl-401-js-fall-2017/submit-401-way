3cbe33597f7b3589cf5145d84852b1ef3dde3717
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-02 14:45:13 -0700

    first 4 operators done

--- test/math.test.js
+++ test/math.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const add = require('../lib/add');
 const subtract = require('../lib/subtract');
 const multiple = require('../lib/multiple');
+const divide = require('../lib/divide');
 
 
 
@@ -16,7 +17,11 @@ describe('math', () => {
     });
 
     it('multiplies two numbers', () => {
-        assert.equal(add(5, 2), 10);
+        assert.equal(multiple(5, 2), 10);
+    });
+
+    it('divides two numbers', () => {
+        assert.equal(divide(6, 2), 3);
     });
 
 });

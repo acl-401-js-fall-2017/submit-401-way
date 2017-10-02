27b64268a557091b2f072bf6aa583ec5b9dee7f1
martypdx <marty@martypdx.com>
2017-10-02 15:57:36 -0700

    add example forEach

---
+++ array-methods.js
@@ -0,0 +1,8 @@
+
+const array = [1, 2, 4];
+
+array.forEach((item, i) => {
+    console.log(item, i);
+});
+
+array.map()
--- test.js
+++
@@ -1,5 +0,0 @@
-const assert = require('assert');
-assert(process.argv);
-
-// eslint-disable-next-line
-console.log('process started');

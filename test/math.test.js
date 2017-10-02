7bf96c3c316ddf6c909b2198c08fc8bb2fe06e8d
Maryussuperboy <martsyalis@gmail.com>
2017-10-02 14:40:22 -0700

    4 tests and functions writen now time to try catch the expeption

--- test/math.test.js
+++ test/math.test.js
@@ -1,8 +1,21 @@
 const assert = require('assert');
 const add = require('../lib/add');
+const subtract = require('../lib/subtract');
+const multiply = require('../lib/multiply');
+const divide = require('../lib/divide');
 
 describe ('math', () => {
     it('adds two numbers', () => {
         assert.equal(add(5, 2), 7);
     });
+    it('subtracts two functions', ()=> {
+        assert.equal(subtract(15, 5),10);
+    });
+    it('multiplies two functions', ()=> {
+        assert.equal(multiply(15, 3),45);
+    });
+    it('divides two functions', ()=> {
+        assert.equal(divide(15, 5),3);
+    });
 });
+

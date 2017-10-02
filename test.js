402e6571b485f0af50b1798cdca146b66a55dc6a
mirrorapps <eli.speigel@gmail.com>
2017-10-02 14:45:40 -0700

    created math function files and linked them to test

---
+++ test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+assert(process.argv);
+
+
+console.log(x);
---
+++ test/math.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const add = require('../lib/add');
+const subtract = require('../lib/subtract');
+const multiply = require('../lib/multiply');
+const divide = require('../lib/divide');
+
+
+describe('math', () => {
+    it('adds two numbers', () => {
+        assert.equal(add(5, 2), 7);
+    });
+    it('finds the difference between numbers', () => {
+        assert.equal(subtract(5, 2), 3);
+    });
+    it('multiples two numbers', () => {
+        assert.equal(multiply(5, 2), 10);
+    });
+    it(' divides a number by another', () => {
+        assert.equal(divide(6, 2), 3);
+    });
+    it.skip('error when dividing by zero', () => {
+        
+    });
+});

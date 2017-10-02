66937ad7c2ed0b9c5e8d629d7be3572c68b1359c
Maryussuperboy <martsyalis@gmail.com>
2017-10-02 15:22:53 -0700

    changed the wording of the test

--- test/math.test.js
+++ test/math.test.js
@@ -8,17 +8,17 @@ describe ('math', () => {
     it('adds two numbers', () => {
         assert.equal(add(5, 2), 7);
     });
-    it('subtracts two functions', ()=> {
+    it('subtracts two numbers', ()=> {
         assert.equal(subtract(15, 5),10);
     });
-    it('multiplies two functions', ()=> {
+    it('multiplies two numbers', ()=> {
         assert.equal(multiply(15, 3),45);
     });
-    it('divides two functions', ()=> {
+    it('divides two numbers', ()=> {
         assert.equal(divide(15, 5),3);
 
     });
-    it('divides by 0', ()=> {
+    it('throws and error when attempting to divide by 0', ()=> {
         try { 
             assert.equal(divide(15, 0),0);
             assert.fail('expected error not thrown');

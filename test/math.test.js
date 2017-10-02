e0ab10ce32843e3bd4b2b4ab5c3348947cbc7d1c
Christina Mills <chrimills@gmail.com>
2017-10-02 14:59:46 -0700

    All four math functions done

--- test/math.test.js
+++ test/math.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const add = require('../lib/add');
 const subtract = require('../lib/subtract')
 const multiply = require('../lib/multiply')
-
+const divide = require('../lib/divide')
 
 describe('math', () => {
     it('adds two numbers', () => {
@@ -17,5 +17,18 @@ describe('math', () => {
         assert.equal(multiply(5, 2), 10)
     });
 
+    it('divides two numbers', () => {
+        assert.equal(divide(6, 2),3)
+    });
+
+    it('error when dividing by 0', () => {
+        try {
+            divide(6, 0);
+            assert.fail('expected error not thrown')
+        }
+        catch (err){
+            assert.equal(err.message, "Attempt to divide by 0")
+        }
+    })
 })
 
